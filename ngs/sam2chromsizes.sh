e938f8e16e3a5a43525d1c82f7dffb92573500c2
Philip Lijnzaad <plijnzaad@gmail.com>
2015-12-08 16:23:47 +0100

    docu

--- ngs/sam2chromsizes.sh
+++ ngs/sam2chromsizes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # -*- mode: sh; -*-
 
-## extract chromosome sizes from sam file
+## extract chromosome sizes from sam/bam file (use '-' to use stdin)
 
 samtools view -S -H "$1" | sed -n '/^@SQ/{s/@SQ.*SN://;s/LN://;p}'
