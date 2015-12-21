0d5edc58b5f5aef44dc559ca229c9328590b49e8
Philip Lijnzaad <plijnzaad@gmail.com>
2015-12-21 18:29:02 +0100

    added arguments and/or chose defaults. Works. Legend still needs work

--- ngs/R/insertlen-distro.R
+++ ngs/R/insertlen-distro.R
@@ -1,8 +1,7 @@
 #!/bin/env Rscript
 
-library(parseargs)
-
 library(uuutils)
+library(parseArgs)
 
 invocation <- paste(commandArgs(), collapse=" ")
 
@@ -11,16 +10,15 @@ print(R.version)
 print(Sys.time())
 print(invocation)
 
-overview <- function(cat(file=stderr(),
-                         "insertlen-distro.R FILENAMES\n
-If a file name argument starts with somename=realfilename.insertlen, somename is used; otherwise,
-the filename without extension is used"))
-
+overview <- function()cat(file=stderr(),
+                         "insertlen-distro.R [ options ] FILENAMES\n\
+If a file name argument looks like realfilename.insertlen=COLOR, that color is used; otherwise,\
+rainbow colors are used.")
   
-args <- parseArgs(dir=NA,               # working directory (where input and chip are taken from)
-                  outdir=".",          # default: same as dir
-                  out="insert-length-distro.pdf",
-                  title='insert lengths',
+args <- parseArgs(out="insert-length-distro.pdf",
+                  title='insert lengths distributions',
+                  maxlen=0L,
+                  scales="1,2,5,10,20,50,100",
                   .overview=overview,
                   .allow.rest=TRUE
                   )
@@ -28,7 +26,8 @@ args <- parseArgs(dir=NA,               # working directory (where input and chi
 print("Arguments:\n")
 print(args)
 print("\n")
-libraries <- c("ngsutils", "NCIS", "rtracklayer", "IRanges", "GenomicRanges")
+
+libraries <- c()
 
 ## load them, and show where they came from:
 for (lib in libraries) { 
@@ -38,78 +37,79 @@ for (lib in libraries) {
 
 files <- args$.rest
 
-data.name <- function(file) {
+file.color <- function(file) {
     re <- "^([^=]+)=([^=]+)$"
     if(grepl(re, file, perl=TRUE)) {
         return(unlist(strsplit(file, "=")))
     }
-    name <- sub("^(.*)\\.[^.]*$", "\\1", file)
-    c(name,file)
-}                                       #data.name
+    c(file,"")
+}                                       #file.color
 
 all.data <- list()
 
-files <-
-  c("Hsf1t0-1A.insertlen", "Hsf1t0-1C.insertlen", "Hsf1t0-2A.insertlen", "Hsf1t0-2C.insertlen", "Hsf1t0-3A.insertlen", "Hsf1t0-3C.insertlen", "Hsf1t30-1A.insertlen", "Hsf1t30-1C.insertlen", "Hsf1t30-2A.insertlen", "Hsf1t30-2C.insertlen", "Hsf1t30-3A.insertlen", "Hsf1t30-3C.insertlen",
-  "Hsf1t0-1B.insertlen", "Hsf1t0-1D.insertlen", "Hsf1t0-2B.insertlen", "Hsf1t0-2D.insertlen", "Hsf1t0-3B.insertlen", "Hsf1t0-3D.insertlen", "Hsf1t30-1B.insertlen", "Hsf1t30-1D.insertlen", "Hsf1t30-2B.insertlen", "Hsf1t30-2D.insertlen", "Hsf1t30-3B.insertlen", "Hsf1t30-3D.insertlen")
+## files <-
+##   c("Hsf1t0-1B.insertlen", "Hsf1t0-1D.insertlen=red", "Hsf1t0-2B.insertlen=blue", "Hsf1t0-2D.insertlen=blue")
 
-##files <-
-##  c("Hsf1t0-1B.insertlen", "Hsf1t0-1D.insertlen", "Hsf1t0-2B.insertlen", "Hsf1t0-2D.insertlen", "Hsf1t0-3B.insertlen", "Hsf1t0-3D.insertlen", "Hsf1t30-1B.insertlen", "Hsf1t30-1D.insertlen", "Hsf1t30-2B.insertlen", "Hsf1t30-2D.insertlen", "Hsf1t30-3B.insertlen", "Hsf1t30-3D.insertlen")
-
-## files <- files[c(4:6)]
-## file <- files[1]
-
+colors <- list()
 
 for(file in files) {
-    name.file <- data.name(file)        #pair of name, file
-    x <- read.table(name.file[2])
-    all.data[[name.file[1]]] <- x[[1]]
+    fc <- file.color(file)        #pair of color, file
+    file <- fc[1]
+    color <- fc[2]
+    name <- sub("^(.*)\\.[^.]*$", "\\1", file) #strip last extension
+    x <- read.table(file)
+    stopifnot(ncol(x)==1)
+    stopifnot(is.integer(x[[1]]))
+    all.data[[name]] <- x[[1]]
+    colors[[name]] <- color
 }
-
-
-densities <- lapply(all.data, density)
-
-maxy <- max(unlist(lapply(densities,function(d)max(d$y))))
 
 col <- rainbow(length(all.data))
 names(col) <- names(all.data)
+for(n in names(colors)) {
+    if (colors[[n]] != "")
+      col[n] <- colors[[n]]
+}
 
-col[ grep("A$", names(col)) ] = "black"
-col[ grep("B$", names(col)) ] = "blue"
-col[ grep("C$", names(col)) ] = "red"
-col[ grep("D$", names(col)) ] = "green4"
+densities <- lapply(all.data, density)
+maxy <- max(unlist(lapply(densities,function(d)max(d$y))))
 
-## out <- sub("\\.pdf$","", out)
-## out <- paste0(out, ".pdf")
-##pdf(file = out, title = title, useDingbats = FALSE, width = 11.7, 
-##    height = 8.3, ...)
+out <- sub("\\.pdf$","", args$out)
+out <- paste0(out, ".pdf")
+title <- args$title
 
-scales <- log10(c(1, 2, 5, 10, 20, 50, 100))
+warning("Creating file ", out)
+pdf(file = out, title = title, useDingbats = FALSE, width = 11.7, 
+    height = 8.3)
+
+scales <- as.integer(unlist(strsplit(args$scales, ",")))
+stopifnot(length(scales)>0 && sum(is.na(scales))==0)
 
 par(mfrow=c(length(scales), 1),
     mar=c(1,5,1,1))
 
-## maxx <- max(unlist(lapply(all.data,max)))
-maxx <- 800
+maxx <- args$maxlen
+if(maxx==0)
+   maxx <- max(unlist(lapply(all.data,max)))
 
 for(scale in scales) { 
     plot(type="n", xlab="",ylab= "",
-         x=c(0, maxx), y=c(0,maxy/(10^scale)),
+         x=c(0, maxx), y=c(0,maxy/scale),
          xaxt="n", yaxt="n")
     s <- seq(0, maxx, 10)
     sl <- s; sl[ sl %% 50 !=0 ] <- NA
     axis(side=1,at=s, labels=sl)
     axis(side=2,labels=FALSE)
-    title(ylab=sprintf("density x %.0f", 10^scale))
-
-    if (scale<= 1e-6) {
+    title(ylab=sprintf("density x %.0f", scale))
+    
+    if (abs(scale - 1) < 1e-6) {
         title(main=title)
         legend(x="topright", legend=names(all.data),
                lty=1, col=col[names(all.data)])
     }
     for(sample in names(all.data)) {
         d <- densities[[sample]]
-        d$y <-  d$y * (10^scale)
+        d$y <-  d$y * scale
         lines(d, col=col[sample])
     }
 }
