4f65e93dc386d05bfd0c43d2247c88d766e3f92f
Philip Lijnzaad <plijnzaad@gmail.com>
2016-02-23 11:36:15 +0100

    nearly there

--- ngs/R/GC.content.per.bin.R
+++ ngs/R/GC.content.per.bin.R
@@ -14,32 +14,39 @@ GC.content.of.genome <- function(genome, binsize=50) {
     ## bins do not overlap. Assumes there is only A, C, G, and T (no N's)
     stopifnot(is(genome, "BSGenome"))
     result <- list()
-    for(chr in names(genome))
-      result[[chr]] <- GC.content.per.bin(genome[[chr]], binsize=binsize)
-    result
+    seqinfo <- Seqinfo(seqnames=seqnames(yeast), seqlengths=seqlengths(yeast), genome="Saccharomyces cerevisiae")
+    granges <- GRanges(seqinfo=seqinfo)
+    for(chr in names(genome)) { 
+        gr <- GC.content.per.bin(dna=genome[[chr]],
+                                 binsize=binsize,
+                                 chr.name=chr,
+                                 seqinfo=seqinfo)
+        result[[chr]] <- gr
+    }
+    do.call("c", result)
 }                                       #GC.content.of.genome
 
 .get.bins <- function(binsize, maxlen) {
     starts <- seq(1, maxlen,by=binsize)
-    ends <- min(starts+binsize-1, maxlen)
+    ends <- starts+binsize-1
+    ends[length(ends)] <- maxlen
     cbind(starts,ends)
 }
 
-GC.content.per.bin <- function(dna, binsize=50) {
+GC.content.per.bin <- function(dna, binsize=50, chr.name, seqinfo) {
     ## bins do not overlap! Assumes there is only A, C, G, and T (no N's)
     ## latter
     stopifnot(is(dna, "DNAString"))
     len <- length(dna)
-    ### bins <- seq(1, len,by=binsize)
     bins <- .get.bins(binsize, len)
     r <- apply(bins, 1, function(row){
-        letterFrequency(dna[row[1]:row[2]], letters=c("C","G"))
-    })
-    ### r <- sapply(bins, function(bin){
-    ###         letterFrequency(dna[bin:(min(bin+binsize-1, len))], letters=c("C","G"))
-    ###     })
+        letterFrequency(dna[row[1]:row[2]], letters=c("C","G"))})
     r <- apply(r, 2,sum)/binsize
-    # correct last bin:
-    r [length(r)] <- r [length(r)]*binsize/(len %% binsize)
-    r
+    r [length(r)] <- r [length(r)]*binsize/(len %% binsize) # correct last bin
+### r
+    GRanges(seqinfo=seqinfo,
+            seqnames=chr.name,
+            ranges=IRanges(start=bins[,1], end=bins[,2]),
+            strand="*",
+            score=r)
 }                                       #GC.content.per.bin
