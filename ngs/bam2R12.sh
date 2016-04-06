246f2e01577fe12269f7390731ec1a07bbd75f50
Philip Lijnzaad <plijnzaad@gmail.com>
2016-04-06 15:17:15 +0200

    bloody dollar

--- ngs/bam2R12.sh
+++ ngs/bam2R12.sh
@@ -5,14 +5,16 @@
 
 bam="$@"
 
-name=$(basename $bam .bam)
+base=$(basename $bam .bam)
 
 ## R1: -f 64
 ## R2: -f 128
 ## unmapped: -f 4
 ## mapped: -F 4
 
-samtools view -h  -f 64 -F 4   $bam -b > $name-R1mapped.bam
-samtools view -h  -f 64 -f 4   $bam -b > $name-R1unmapped.bam
-samtools view -h  -f 128 -F 4   $bam -b > $name-R2mapped.bam
-samtools view -h  -f 128 -f 4   $bam -b > $name-R2unmapped.bam
+resources="-l h_rt=0:30:00,h_vmem=1G"
+
+name="$base-R1mapped";   qrun.sh -N $name  $resources "samtools view -h  -f 64 -F 4   $bam -b > $name.bam"
+name="$base-R1unmapped"; qrun.sh -N $name  $resources "samtools view -h  -f 64 -f 4   $bam -b > $name.bam"
+name="$base-R2mapped";   qrun.sh -N $name  $resources "samtools view -h  -f 128 -F 4   $bam -b > $name.bam"
+name="$base-R2unmapped"; qrun.sh -N $name  $resources "samtools view -h  -f 128 -f 4   $bam -b > $name.bam"
