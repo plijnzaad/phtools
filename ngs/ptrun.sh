1de65c4c2f9773bea3a611afa7a86541b8366ec4
Philip Lijnzaad <plijnzaad@gmail.com>
2016-04-08 14:56:50 +0200

    overhaul, now most is done in the lua modules file using a _pt* environment variables

--- ngs/ptrun.sh
+++ ngs/ptrun.sh
@@ -1,59 +1,13 @@
 #!/bin/bash
 # -*- mode: sh; -*-
+## little wrapper script for  the picard-tools, to be used in conjunction with the alias in module.lua file
+## (see also ptrun-lmod.lua.eg)
+## which takes care of using the right jar, memory and tmpdir
 
-### little wrapper script for the picard-tools (see
-### http://sourceforge.net/projects/picard/)
-
-### To get help on options specific to the subcommand, just 
-### do ptrun.sh subcommand. For help on options common to 
-### all subcommands, do ptrun subcommand --stdhelp. 
-
-### Note that you can often use INPUT=/dev/stdin for reading from std input, and
-### OUPUT=/devstdout QUIET=true for writing to stdout.
-
-### VALIDATION_STRINGENCY=LENIENT is always used.
-
-echo 'Prolly obsolete with newer PicardToolses. Consider something like this (lmod specification of an alias):
-  set_alias("ptrun", "env VALIDATION_STRINGENCY=LENIENT java -Xmx4G -jar " .. pathJoin(base, "picard.jar"))' >&2
-exit 7
-
-validation='VALIDATION_STRINGENCY=LENIENT'
-
-## avoid "ERROR: Option 'VALIDATION_STRINGENCY' cannot be specified more than once."
-for i in "$@"; do 
-    if [ "$i" == "$validation" ]; then
-        validation=""
-    fi
-done
-
-pthelp() {
-  pushd $pthome
-  echo "
-  Usage: ptrun.sh jarfile, where FILE is one of:
-"
-  ls -x *.jar | sed 's/\.jar//g'
-  exit 1
-}
-
-require_var() { 
-    for var in "$@"; do
-      eval "val=\$$var"
-      if [ -z "$val"  ]; then
-          echo "variable $var is not defined; exiting" >&2
-          return 3
-      fi
-    done
-}
-
-require_var pthome
-## e.g. /usr/local/picard-tools
-
-if [ $# -lt 1 ] ; then
-        pthelp
+if [ x$_ptjar = x -o ! -f $_ptjar ] ; then # defined in the 
+  echo "picard tools jar file not defined or not found" >&2
+  exit 8
 fi
 
-jar="$1"
-shift
-
-java -d64 -Xmx512M -jar $pthome/$jar.jar "$@" $validation
+java $_pt_javaopts -jar $_ptjar "$@"  $_ptdefaults
 
