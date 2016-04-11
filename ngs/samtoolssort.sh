8b89d305462a28fa09375cbb22445e174a399499
Philip Lijnzaad <plijnzaad@gmail.com>
2016-04-11 13:51:38 +0200

    comments as docu

--- ngs/samtoolssort.sh
+++ ngs/samtoolssort.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # -*- mode: sh; -*-
-
+## This script is just so that the queueing system can supply the $TMPDIR
+## Typically used as  
+## 
+##  ... | samtools sort -@ 4  -Obam > x.bam
+## 
 samtools sort -T$TMPDIR/sort "$@"
