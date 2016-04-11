4107f345c9e6b84fde364dd30dd6136c50c9bcef
Philip Lijnzaad <plijnzaad@gmail.com>
2016-04-11 13:00:09 +0200

    reformat

--- ngs/ptrun.sh
+++ ngs/ptrun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # -*- mode: sh; -*-
-## little wrapper script for  the picard-tools, to be used in conjunction with the alias in module.lua file
-## (see also ptrun-lmod.lua.eg)
-## which takes care of using the right jar, memory and tmpdir
+## little wrapper script for the picard-tools, to be used in conjunction
+## with the alias in module.lua file (see also ptrun-lmod.lua.eg) which
+## takes care of using the right jar, memory and tmpdir
 
 if [ x$_pt_jar = x]; then
   echo "_pt_jar variable not defined" >&2
