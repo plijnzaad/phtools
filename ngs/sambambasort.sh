c02ed4423b4c5aaa0d6fac2b0e202a4fcbc7bd73
Philip Lijnzaad <plijnzaad@gmail.com>
2016-08-11 16:23:45 +0200

    docu

--- ngs/sambambasort.sh
+++ ngs/sambambasort.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # -*- mode: sh; -*-
 ## drop-in replacement of sambamba sort that makes use of the proper local TMPDIR on the compute node
-sambamba sort --tmpdir=$TMPDIR "$@"
+##
+## Usage in pipeline e.g.: 
+##    somepipeline |  sambambasort.sh --nthreads=4 --memory-limit=2GB /dev/stdin -o stuff.bam
+##
 ## (NOTE: needs a bam file, not a sam file!)
+##
+sambamba sort --tmpdir=$TMPDIR "$@"
