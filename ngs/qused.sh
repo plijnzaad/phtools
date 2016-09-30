47a35b1a127112d0710d3b6bb07ae6ce8ecf460c
Philip Lijnzaad <plijnzaad@gmail.com>
2016-09-30 11:28:39 +0200

    usage message when no arg is given

--- ngs/qused.sh
+++ ngs/qused.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 # -*- mode: sh; -*-
-# show statistics of a past job
+# show statistics of a past job. Note that this may take fairly long
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 jobid" >&2
+    exit 2
+fi
+
 qacct -j  $@  | egrep '^(jobname|jobnumber|ru_wallclock|mem|maxvmem|io|jobargs|submission_time)'
