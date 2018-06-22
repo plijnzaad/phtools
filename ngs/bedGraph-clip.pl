36bab7aef0966365f6743439d9c80d3a376f1d26
Philip Lijnzaad <plijnzaad@gmail.com>
2018-06-22 13:08:32 +0200

    written for Wim

---
+++ ngs/bedGraph-clip.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+use strict;
+
+# script to clip content of bedGraph files that lie beyond the specified sizes
+
+my $usage = "Usage: gunzip< sample.bedgraph.gz | bedGraph-clip.pl chromsizes.txt | gzip > sample-clipped.bedgraph.gz\n";
+
+die $usage unless @ARGV==1;
+
+my $table=shift;
+
+open(TAB, $table) || die "$table: $!";
+
+my $chrlens={};
+
+CHR:
+while(<TAB>) { 
+  s/#.*//;
+  next CHR unless /(\S+)\s+(\d+)/;
+  my ($chr, $len)= ($1,$2);
+  $chrlens->{$chr}=$len;
+}
+close(TAB);
+
+my $nlines=0;
+my $nclipped=0;
+
+LINE:
+while(<>) {
+  if (/^track/) {
+    print;
+    next LINE;
+  }
+  $nlines++;
+  my ($chr,$start, $end,$value)=split("\t", $_);
+  my $len = $chrlens->{$chr};
+  die "Unknown chromosome '$chr'" unless defined($len);
+  if ($start > $len || $end > $len) {
+    $nclipped++;
+    next LINE;
+  }
+  print;
+}
+warn sprintf("Clipped %d out %d lines (%.1f%%)\n", $nclipped, $nlines, 100*$nclipped/$nlines);
