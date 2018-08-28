907c3f060d258c7ba21633aae8a248305b2db0cb
Philip Lijnzaad <plijnzaad@gmail.com>
2018-08-28 13:27:44 +0200

    docu

--- ngs/sambambasort.sh
+++ ngs/sambambasort.sh
@@ -4,7 +4,12 @@
 ## that makes use of the proper local TMPDIR on the compute node. See also 
 ## the Makefile in this directory.
 ##
+## Using given bamfile, e.g.:
+##
+##    sambambasort.sh --nthreads=4 --memory-limit=2GB stuff.bam
+##
 ## Usage in pipeline e.g.: 
+##
 ##    somepipeline |  sambambasort.sh --nthreads=4 --memory-limit=2GB /dev/stdin -o stuff.bam
 ##
 ## (NOTE: needs a bam file, not a sam file!)
